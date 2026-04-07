ca9cbed441951688812ec23c912fa4b61865ea2f
johngodlee <johngodlee@gmail.com>
2026-04-07 10:09:12 -0400

    Initial testing of non-spatial newPlotSelect

--- R/newPlotSelect.R
+++ R/newPlotSelect.R
@@ -27,80 +27,109 @@
 #' 
 #' @export
 #'
-
-newPlotSelect <- function(r, p, n_plots, p_new_dim = NULL, r_mask = NULL, 
+newPlotSelect <- function(r, p = NULL, n_plots, p_new_dim = NULL, r_mask = NULL, 
   pca = TRUE, n_pca = NULL, method = meanminSelect) {
 
-  # If new plot dimensions not provided, set to raster resolution
-  if (is.null(p_new_dim)) {
-    p_new_dim <- res(r)
+  # Check type of r and check inputs
+  if (!inherits(r, "SpatRaster")) {
+    if (!inherits(r, "data.frame")) { 
+      stop("if `r` is not a 'SpatRaster' it must be a 'data.frame'")
+    }
+
+    if (!is.null(p)) { 
+      if (!is.numeric(p) || !is.vector(p) || any(is.na(p)) || 
+        any(p > nrow(r)) || any(p < 1) || !all(p == floor(p))) {
+      message("if `r` is not a 'SpatRaster', `p` must be a vector of row indices in `r`.")
+      }
+    }
+
+    if (!is.null(p_new_dim)) { 
+      message("if `r` is not a 'SpatRaster', `p_new_dim` is ignored.")
+      p_new_dim <- NULL
+    }
+
+    if (!is.null(r_mask)) { 
+      message("if `r` is not a 'SpatRaster', `r_mask` is ignored.")
+      r_mask <- NULL
+    }
   }
 
-  # If new plot dimensions are smaller than the raster resolution
-  if (any(p_new_dim < res(r))) {
-    warning("'p_new_dim' is smaller than the resolution of 'r'.\nSetting 'p_new_dim' to the resolution of 'r'\n",
-      immediate. = TRUE)
-    p_new_dim <- res(r)
-  }
+  # If r is a raster
+  if (inherits(r, "SpatRaster")) { 
+    # If new plot dimensions not provided, set to raster resolution
+    if (is.null(p_new_dim)) {
+      p_new_dim <- res(r)
+    }
 
-  if (any(p_new_dim %% res(r) != 0)) {
-    p_new_dim <- floor(p_new_dim / res(r)) * res(r)
-    warning("'p_new_dim' is not perfectly divisible by the resolution of 'r'.\nRounding 'p_new_dim' down a value which is evenly divisible: ", paste(p_new_dim, collapse = ", "))
-  }
+    # If new plot dimensions are smaller than the raster resolution
+    if (any(p_new_dim < res(r))) {
+      warning("'p_new_dim' is smaller than the resolution of 'r'.\nSetting 'p_new_dim' to the resolution of 'r'\n",
+        immediate. = TRUE)
+      p_new_dim <- res(r)
+    }
 
-  # If new plot dimensions are just a single value, multiply it up
-  if (length(p_new_dim) == 1) {
-    p_new_dim <- rep(p_new_dim, 2)
-  }
+    if (any(p_new_dim %% res(r) != 0)) {
+      p_new_dim <- floor(p_new_dim / res(r)) * res(r)
+      warning("'p_new_dim' is not perfectly divisible by the resolution of 'r'.\nRounding 'p_new_dim' down a value which is evenly divisible: ", paste(p_new_dim, collapse = ", "))
+    }
 
-  # If mask for new plots is provided, mask the raster
-  if (!is.null(r_mask)) { 
-    r_mask <- terra::mask(r, r_mask)
-  } else {
-    r_mask <- r
-  }
+    # If new plot dimensions are just a single value, multiply it up
+    if (length(p_new_dim) == 1) {
+      p_new_dim <- rep(p_new_dim, 2)
+    }
 
-  # Convert p to spatial vector object 
-  # Extract metrics for each plot polygon
-  if (!is.null(p)) {
-    old_ext <- extractPlotMetrics(r, p)
-  } else {
-    old_ext <- NULL
-  }
+    # If mask for new plots is provided, mask the raster
+    if (!is.null(r_mask)) { 
+      r_mask <- terra::mask(r, r_mask)
+    } else {
+      r_mask <- r
+    }
+
+    # Convert p to spatial vector object 
+    # Extract metrics for each plot polygon
+    if (!is.null(p)) {
+      old_ext <- extractPlotMetrics(r, p)
+    } else {
+      old_ext <- NULL
+    }
   
-  # Optional PCA to reduce dimensionality
-  if (pca && is.null(n_pca)) {
-   n_pca <- ncol(r)
-  }
-
-  if (pca) { 
-    old_pca <- PCALandscape(r, old_ext, center = TRUE, scale. = TRUE)
-    r_pca <- rep(r[[1]], n_pca)
-    terra::values(r_pca)[complete.cases(terra::values(r)),] <- old_pca$r_pca$x[,1:n_pca]
-    names(r_pca) <- colnames(old_pca$r_pca$x)[1:n_pca]
-    if (!is.null(p)) {
-      p_pca <- old_pca$p_pca[,1:n_pca]
-    } else {
-      p_pca <- NULL
+    # Optional PCA to reduce dimensionality
+    if (pca && is.null(n_pca)) {
+     n_pca <- ncol(r)
     }
-  } else {
-    r_pca <- scale(r)
-    if (!is.null(p)) {
-      p_pca <- old_ext[,names(r_pca)]
+
+    if (pca) { 
+      old_pca <- PCALandscape(r, old_ext, center = TRUE, scale. = TRUE)
+      r_pca <- rep(r[[1]], n_pca)
+      terra::values(r_pca)[complete.cases(terra::values(r)),] <- old_pca$r_pca$x[,1:n_pca]
+      names(r_pca) <- colnames(old_pca$r_pca$x)[1:n_pca]
+      if (!is.null(p)) {
+        p_pca <- old_pca$p_pca[,1:n_pca]
+      } else {
+        p_pca <- NULL
+      }
     } else {
-      p_pca <- NULL
+      r_pca <- scale(r)
+      if (!is.null(p)) {
+        p_pca <- old_ext[,names(r_pca)]
+      } else {
+        p_pca <- NULL
+      }
     }
-  }
 
-  # Get index values in PCA for existing plots and potential new plots
-  if (!is.null(p)) {
-    old_ind <- which(complete.cases(terra::values(terra::mask(r, terra::vect(p)))))
+    # Get index values in PCA for existing plots and potential new plots
+    if (!is.null(p)) {
+      old_ind <- which(complete.cases(terra::values(terra::mask(r, terra::vect(p)))))
+    } else {
+      old_ind <- NULL
+    }
+
+    # Initialise PCA values for candidate plots 
+    new_ind <- which(complete.cases(terra::values(r_mask)))
   } else {
-    old_ind <- NULL
+    # TODO
   }
 
-  # Initialise PCA values for candidate plots 
-  new_ind <- which(complete.cases(terra::values(r_mask)))
 
   # Implement plot selection algorithm
   p_list <- method(
